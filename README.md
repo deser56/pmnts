c7f9722d965740877acf12c1fbb940aea1061e4d
deser56 <86250972+deser56@users.noreply.github.com>
2023-07-04 20:16:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pmnts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ekegjy)
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ObjectId } = require('mongodb');
+
+const connectionString = 'mongodb+srv://your-connection-string';
+const dbName = 'your-database-name';
+const collectionName = 'payments';
+
+let db;
+
+async function connectToDatabase() {
+  try {
+    const client = new MongoClient(connectionString);
+    await client.connect();
+    db = client.db(dbName);
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
+app.use(express.json());
+
+app.get('/payments', async (req, res) => {
+  try {
+    const payments = await db.collection(collectionName).find().toArray();
+    res.json(payments);
+  } catch (error) {
+    console.error('Error retrieving payments:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/payments/:id', async (req, res) => {
+  const paymentId = req.params.id;
+  try {
+    const payment = await db.collection(collectionName).findOne({
+      _id: ObjectId(paymentId),
+    });
+    if (payment) {
+      res.json(payment);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error('Error retrieving payment:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/payments', async (req, res) => {
+  const { orderId, amount, currency } = req.body;
+  try {
+    const result = await db.collection(collectionName).insertOne({
+      orderId,
+      amount,
+      currency,
+      status: 'Pending',
+    });
+    res.status(201).json({ _id: result.insertedId });
+  } catch (error) {
+    console.error('Error creating payment:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/payments/:id', async (req, res) => {
+  const paymentId = req.params.id;
+  const { status } = req.body;
+  try {
+    const result = await db.collection(collectionName).updateOne(
+      { _id: ObjectId(paymentId) },
+      { $set: { status } }
+    );
+    if (result.modifiedCount > 0) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error('Error updating payment:', error);
+    res.sendStatus(500);
+  }
+});
+
+connectToDatabase().then(() => {
+  app.listen(3000, () => {
+    console.log('Payment service is running on port 3000');
+  });
+});
+
